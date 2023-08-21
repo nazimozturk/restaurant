6f53d70f6486ed30fdde559b3a9276de3b09701c
Nazım ÖZTÜRK <81512577+nazimozturk@users.noreply.github.com>
2023-08-21 18:20:26 +0300

    UpdateComment

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -8,6 +8,7 @@ const CategoryPage = () => {
     <div className="flex flex-wrap text-red-500">
       {pizzas.map((item) => (
         <Link
+          // "odd" çift sayılar için "even" tek sayılar için arka plan rengi ayarlıyor
           className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group even:bg-fuchsia-50"
           href={`/product/${item.id}`}
           key={item.id}
